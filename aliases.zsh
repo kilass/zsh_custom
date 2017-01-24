8dbfb0079083877991a6c55d5cba0fc8f0de2d78
Jérémy SEBAN <jeremy@seban.eu>
2017-01-24 13:33:29 +0100

    Latest updated

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,4 @@
 #### Aliases
 alias ls='ls --color'
+alias global_geth='sudo geth attach ipc:/var/lib/geth/geth.ipc'
+alias private_geth='sudo geth attach ipc:/home/mechsloth/Projects/blockchain_wip/geth_container/bc_test/eth_data/geth.ipc'
